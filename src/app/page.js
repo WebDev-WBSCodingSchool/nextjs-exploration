f646380b632d3bc3f3d6b4f98c1536c63b62e424
Jorge Paul <jserranopaul@gmail.com>
2025-06-16 23:49:09 +0200

    Cleanup

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,9 @@
-import Link from 'next/link';
-import Counter from '@/app/components/Counter';
+import { Suspense } from 'react';
+import { Counter, PostsList, PostsListSkeleton } from '@/components';
+import { getPosts } from '@/data/posts';
 
-export default async function Home() {
-  const res = await fetch('https://jsonplaceholder.typicode.com/posts');
-  if (!res.ok) throw new Error('Failed to fetch posts');
-  const posts = await res.json();
-
-  console.log('This log is from the server side');
+export default function Home() {
+  const postsPromise = getPosts();
 
   return (
     <main className='p-4 space-y-4'>
@@ -15,16 +12,10 @@ export default async function Home() {
         <Counter />
       </section>
       <section>
-        <ul className='space-y-2'>
-          {posts.map(post => (
-            <Link href={`/post/${post.id}`} key={post.id}>
-              <li className='border p-2 rounded mb-5'>
-                <h2 className='font-semibold'>{post.title}</h2>
-                <p>{post.body}</p>
-              </li>
-            </Link>
-          ))}
-        </ul>
+        <h2 className='text-xl font-semibold'>Posts</h2>
+        <Suspense fallback={<PostsListSkeleton />}>
+          <PostsList postsPromise={postsPromise} />
+        </Suspense>
       </section>
       <h1 className='text-2xl font-bold'>Latest Posts</h1>
     </main>
