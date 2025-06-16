356341acbb19d8ded839bbcee5e34a21312f1746
Jorge Paul <jserranopaul@gmail.com>
2025-06-16 23:19:53 +0200

    Home

--- src/app/page.js
+++ src/app/page.js
@@ -1,17 +1,32 @@
 import Link from 'next/link';
+import Counter from '@/app/components/Counter';
 
-export default function Home() {
+export default async function Home() {
+  const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+  if (!res.ok) throw new Error('Failed to fetch posts');
+  const posts = await res.json();
+
+  console.log('This log is from the server side');
+
   return (
     <main className='p-4 space-y-4'>
+      <section>
+        <h2 className='text-xl font-semibold'>Try the Counter</h2>
+        <Counter />
+      </section>
+      <section>
+        <ul className='space-y-2'>
+          {posts.map(post => (
+            <Link href={`/post/${post.id}`} key={post.id}>
+              <li className='border p-2 rounded mb-5'>
+                <h2 className='font-semibold'>{post.title}</h2>
+                <p>{post.body}</p>
+              </li>
+            </Link>
+          ))}
+        </ul>
+      </section>
       <h1 className='text-2xl font-bold'>Latest Posts</h1>
-      {Array.from({ length: 5 }, (_, i) => (
-        <Link href={`/post/${i + 1}`} key={i}>
-          <div className='border p-2 rounded mb-5'>
-            <h2 className='font-semibold'>Post {i + 1}</h2>
-            <p className='text-gray-700'>This is a brief description of post {i + 1}.</p>
-          </div>
-        </Link>
-      ))}
     </main>
   );
 }
